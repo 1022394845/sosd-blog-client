321f4f77416f3f659dd73ba603eb68bbef0442c9
1022394845 <1022394845@qq.com>
2025-08-29 12:46:03 +0800

    项目初始化

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}']
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+
+  eslintPluginPrettierRecommended,
+
+  {
+    rules: {
+      // 使用插件提供的规则（格式：`插件名/规则名`）
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'] // 关闭 props 解构的校验
+    }
+  }
+])
