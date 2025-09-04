bf2a23f2835653b69b461f4e689b976e27fb3b4f
1022394845 <1022394845@qq.com>
2025-09-04 15:00:25 +0800

    新建文章逻辑完善

--- src/views/User/views/Publish/index.vue
+++ src/views/User/views/Publish/index.vue
@@ -5,11 +5,9 @@ import RichTextEditor from '@/components/RichTextEditor.vue'
 import { useArticleStore } from '@/stores/article'
 import { useUserStore } from '@/stores/user'
 import { showMsg } from '@/utils/common'
-import { ref, useTemplateRef, onMounted, computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, useTemplateRef, onMounted, computed, watch } from 'vue'
 
 const userStore = useUserStore()
-const router = useRouter()
 
 // 表单
 const formRef = useTemplateRef('formRef')
@@ -20,7 +18,7 @@ const formData = ref({
   image: undefined,
   categoryId: null,
   tags: [],
-  content: undefined
+  content: '<p><br></p>' // 避免初始化时影响watch
 })
 // 去除rich-text-editor保留的空行
 const trimContent = computed(() => {
@@ -59,9 +57,15 @@ onMounted(() => {
   getTagList()
 })
 
-// 提交表单
+const allowPublish = ref(false)
 const btnDisabled = ref(false)
-const onSubmit = async () => {
+/**
+ * 提交表单
+ * @param {Boolean} publish 保存后自动提交 默认false
+ */
+const onSubmit = async (publish = false) => {
+  allowPublish.value = publish
+
   // 校验表单
   try {
     const result = await formRef.value.validate()
@@ -78,29 +82,70 @@ const onSubmit = async () => {
   }
 }
 
-// 发布文章
+let needSave = false // 需要保存
+let stopWatch = null
+// 创建修改监听器
+const createWatcher = () => {
+  if (stopWatch) stopWatch() // 销毁已存在的监听器
+  stopWatch = watch(
+    formData,
+    () => {
+      needSave = true
+      stopWatch() // 停止监听
+      stopWatch = null
+    },
+    {
+      deep: true
+    }
+  )
+}
+onMounted(() => {
+  createWatcher()
+})
+
+// 保存文章
 const loading = ref(false)
-const handlePublish = async () => {
+const handleSave = async () => {
   loading.value = true
 
-  const data = {
-    ...formData.value,
-    content: trimContent.value
-  }
   try {
-    if (data.id) {
-      // 编辑文章
-    } else {
-      // 新建文章
-      await createArticleAPI(userStore.getCurrentUserId(), data)
-      showMsg('新建成功', 'success')
+    if (needSave) {
+      // 需要保存文章内容
+      const data = {
+        ...formData.value,
+        content: trimContent.value
+      }
+
+      if (data.id) {
+        // 编辑文章
+        showMsg('保存成功', 'success')
+      } else {
+        // 新建文章
+        const { articleId } = await createArticleAPI(
+          userStore.getCurrentUserId(),
+          data
+        )
+        if (typeof articleId === 'number') formData.value.id = articleId
+        showMsg('新建成功', 'success')
+      }
+      needSave = false
+      createWatcher() // 创建修改监听器
     }
 
-    // router.replace('/user/article')
+    if (allowPublish.value) {
+      handlePublish() // 直接发布文章
+      return // 跳过解除loading
+    }
   } finally {
     loading.value = false
     btnDisabled.value = false
   }
+}
+
+// 发布文章
+const handlePublish = () => {
+  console.log('publish')
+  allowPublish.value = false
 }
 </script>
 
@@ -148,7 +193,7 @@ const handlePublish = async () => {
             ref="uploadRef"
             v-model="formData.image"
             size="min(148px, 30vw)"
-            @success="handlePublish"
+            @success="handleSave"
           ></image-uploader>
         </el-form-item>
         <el-form-item label="内容" prop="content">
@@ -156,24 +201,46 @@ const handlePublish = async () => {
         </el-form-item>
       </el-form>
     </div>
-    <el-button
-      class="submit-btn gradient-1"
-      :loading="btnDisabled"
-      @click="onSubmit"
-    >
-      发布
-    </el-button>
+    <div class="btn-group">
+      <el-button
+        class="submit-btn gradient-1"
+        :loading="btnDisabled"
+        @click="onSubmit"
+      >
+        {{ isNaN(formData.id) ? '新建' : '保存' }}
+      </el-button>
+      <el-button
+        class="submit-btn gradient-2"
+        v-if="isNaN(formData.id) || allowPublish"
+        :loading="btnDisabled"
+        @click="onSubmit(true)"
+      >
+        发布
+      </el-button>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .page-container {
   padding: 20px;
+}
+
+.btn-group {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px 40px;
+  width: 70%;
+  margin: 40px auto 10px;
 
   .submit-btn {
-    width: min(300px, 70%);
+    flex: 1;
+    min-width: 160px;
+    max-width: 300px;
     height: 40px;
-    margin: 40px auto 10px;
+    margin-left: 0;
     font-size: 16px;
   }
 }
